fdec744f1d6f17763249d15ef78ad2ff83408c51
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2025-03-24 08:50:21 +0100

    feat: setup

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col items-center justify-center gap-4 h-screen">
+    <h1 class="font-bold text-2xl text-(--ui-primary)">
+      Nuxt UI - Starter
+    </h1>
+
+    <UCard>
+      <template #header>
+        tester
+      </template>
+      <template #footer>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae hic at, excepturi cupiditate reprehenderit officiis, debitis vel ea magni esse assumenda vitae, sunt non nulla! Qui modi magni ipsa assumenda.
+      </template>
+    </UCard>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/image',
+  ],
+  devtools: { enabled: true },
+
+  css: ['~/assets/css/main.css'],
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  compatibilityDate: '2024-11-27',
+
+  eslint: { config: { stylistic: true } },
+})
